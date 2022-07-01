70ba886657f81f94bc6305c2cbddbda7cc9e2e01
xiaowenjia <385906070@qq.com>
2022-07-01 16:57:39 +0800

    number.3 添加动态规划,优化速度

--- 3.无重复字符的最长子串/solution1.js
+++ 3.无重复字符的最长子串/solution1.js
@@ -3,30 +3,26 @@
  * @return {number}
  */
 module.exports = function lengthOfLongestSubstring(s) {
-    let curSubStr = '',
-        over = false
+    let maxSubStr = '',
+        curSubStr = ''
     const strLen = s.length
-    for (let i = 0; i < strLen; ++i) {
-        if (over) {
-            break
-        }
-        for (let j = i + 1; j < strLen + 1; ++j) {
-            const str = s.substring(i, j)
+    for (let start = 0; start < strLen; ++start) {
+        // 动态规划：end = start + curSubStr.length，因为之前的子串肯定不重复，end 直接跳过之前的子串
+        for (let end = start + curSubStr.length; end < strLen + 1; ++end) {
+            const str = s.substring(start, end)
             // console.log(str)
             if (isNoRepeatString(str)) {
-                if (str.length > curSubStr.length) {
-                    curSubStr = str
-                }
-                if (j == strLen) {
-                    over = true
+                if (str.length > maxSubStr.length) {
+                    maxSubStr = str
                 }
             } else {
+                curSubStr = str.substring(0, str.length - 1)
                 break
             }
         }
     }
-    // console.log(curSubStr)
-    return curSubStr.length
+    // console.log(maxSubStr)
+    return maxSubStr.length
 }
 
 function isNoRepeatString(str) {
@@ -35,6 +31,6 @@ function isNoRepeatString(str) {
 
 /**
  * 暴力求解
- * 1. 设定 i 为左索引，j 为右索引，curSubStr 为当前最长子串
- * 2. 通过不断自增 i 和 j，截取中间的子串，如果比 curSubStr 长则记录，直到 i 和 j 遍历结束
+ * 1. 设定 start 为开始索引，end 为结束索引，curSubStr 为当前最长子串
+ * 2. 通过不断自增 start 和 end，截取中间的子串，如果比 curSubStr 长则记录，直到 start 和 end 遍历结束
  */
