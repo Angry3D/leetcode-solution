225f708c0a5e3d1c5751d0bced2d4fd1f800409d
xiaowenjia <385906070@qq.com>
2022-07-02 16:16:11 +0800

    添加 num.5 及其 solution1

---
+++ 5.最长回文子串【中等】/solution1.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+module.exports = function longestPalindrome(s) {
+    if (s.length <= 1) {
+        return s
+    }
+    const strLen = s.length,
+        sAry = s.split('')
+    let start = 0,
+        maxLen = 1
+    for (let i = 0; i < strLen; ++i) {
+        for (let j = i + 1; j < strLen + 1; ++j) {
+            if (isPalindromeStr(sAry, i, j) && j - i + 1 > maxLen) {
+                start = i
+                maxLen = j - i + 1
+            }
+        }
+    }
+    // console.log(start, maxLen)
+    return s.substring(start, start + maxLen)
+}
+
+// 判读是否为回文
+function isPalindromeStr(sAry, i, j) {
+    while (i < j) {
+        if (sAry[i] == sAry[j]) {
+            ++i
+            --j
+        } else {
+            break
+        }
+    }
+    return i >= j
+}
+
+/**
+ * 暴力求解
+ */
---
+++ 5.最长回文子串【中等】/test.js
@@ -0,0 +1,8 @@
+const s =
+    'aa'
+
+// solution1
+const solution1 = require('./solution1')
+console.time('solution1')
+console.log(solution1(s))
+console.timeEnd('solution1')
