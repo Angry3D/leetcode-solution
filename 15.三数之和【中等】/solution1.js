953cb2141c945915d6cadd8f722dee36ff9ffb97
xiaowenjia <385906070@qq.com>
2022-08-06 23:06:42 +0800

    修改题目文件夹名称

---
+++ 15.三数之和【中等】/solution1.js
@@ -0,0 +1,5 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+module.exports = function threeSum(nums) {}
---
+++ 15.三数之和【中等】/test.js
@@ -0,0 +1,7 @@
+const nums = [-1,0,1,2,-1,-4]
+
+// solution1
+const solution1 = require('./solution1')
+console.time('solution1')
+console.log(solution1(nums))
+console.timeEnd('solution1')
