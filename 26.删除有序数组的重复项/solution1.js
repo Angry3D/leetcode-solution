f6988b5f36fd6ee481065d4d095426e715e787d3
xiaowenjia <385906070@qq.com>
2023-07-17 18:03:49 +0800

    add 26

---
+++ 26.删除有序数组的重复项/solution1.js
@@ -0,0 +1,5 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+module.exports = function removeDuplicates(nums) {}
---
+++ 26.删除有序数组的重复项/test.js
@@ -0,0 +1,5 @@
+const utils = require('../utils')
+
+const nums = [1, 1, 2]
+
+utils.test('solution1', nums)
