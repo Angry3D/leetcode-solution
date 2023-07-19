6886b06ebf4d0262528d4a185f12cb2832dfe00c
xiaowenjia <385906070@qq.com>
2023-07-19 11:35:08 +0800

    add 29

---
+++ 29.两数相除【中等】/solution1.js
@@ -0,0 +1,6 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+module.exports = function divide(dividend, divisor) {}
---
+++ 29.两数相除【中等】/test.js
@@ -0,0 +1,6 @@
+const utils = require('../utils')
+
+const dividend = 10,
+  divisor = 3
+
+utils.test('solution1', dividend, divisor)
