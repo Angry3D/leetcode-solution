8cad1243475adfe14bce2ab6067c2bae4773ab65
xiaowenjia <385906070@qq.com>
2023-08-09 10:49:04 +0800

    add num.31

---
+++ 31.下一个排列【中等】/solution1.js
@@ -0,0 +1,5 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+module.exports = function nextPermutation(nums) {}
---
+++ 31.下一个排列【中等】/test.js
@@ -0,0 +1,5 @@
+const utils = require('../utils')
+
+const nums = [1, 2, 3]
+
+utils.test('solution1', nums)
