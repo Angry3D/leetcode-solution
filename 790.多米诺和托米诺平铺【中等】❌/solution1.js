b6f70ea2bdae9b48a574aa38d87d0b513a60d256
xiaowenjia <385906070@qq.com>
2025-09-01 17:13:02 +0800

    feat: add ques.790

---
+++ 790.多米诺和托米诺平铺【中等】❌/solution1.js
@@ -0,0 +1,5 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+module.exports = function numTilings(n) {};
---
+++ 790.多米诺和托米诺平铺【中等】❌/test.js
@@ -0,0 +1,4 @@
+const utils = require('../utils');
+
+utils.test('solution1', 3);
+// utils.test('solution1', 1);
